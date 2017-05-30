ec4f2195637fb5c52ef266a83d0678bad7c8ec7c
Nguy?n T?n Quang <tanquanga9@gmail.com>
2017-05-30 19:32:57 +0700

    Tao class Main

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,10 +1,18 @@
 package test;
 
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Admin on 30/05/2017.
  */
 public class Main {
     public static void main(String[] args) {
-        
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld1");
+        helloWorld.print();
+
     }
 }
