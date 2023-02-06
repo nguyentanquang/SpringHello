abcf9a438ef2fea6ad70922957a74ab072cb99aa
Nguyễn Tấn Quang <tanquanga9@gmail.com>
2023-02-06 22:39:48 +0700

    Update HelloWorld.java

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -15,3 +15,13 @@ echo "item3:value3" >> config
 # Call the find function and pass in the item name as an argument
 result=$(find "item2")
 echo "Value of item2: $result"
+
+
+function parse_json {
+  local json_file=$1
+  local field_name=$2
+  local value=$(grep -oE "\"$field_name\":\"[^\"]+\"" $json_file | awk -F":" '{print $2}' | tr -d '"')
+  echo "Value of $field_name is: $value"
+}
+
+parse_json "file.json" "field_name"
