01ac86f6b028f9e3d647cbea22ddb7e657d0e0c1
Nguyễn Tấn Quang <tanquanga9@gmail.com>
2023-02-22 14:07:24 +0700

    Update HelloWorld.java

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -1,3 +1,40 @@
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { Construct } from 'constructs';
+
+interface StackProps {
+  prefix: string
+  cidr: string
+}
+
+/**
+ * Creates a new custom VPC
+ *
+ * @param  {cdk.Construct} scope stack application scope
+ * @param  {StackProps} props props needed to create the resource
+ *
+ */
+export class CustomVPC {
+  public readonly vpc: ec2.IVpc
+
+  constructor(scope: Construct, props: StackProps) {
+    this.vpc = new ec2.Vpc(scope, `${props.prefix}-vpc`, {
+      maxAzs: 1, // RDS requires at least 2
+      cidr: props.cidr, // the ip address block of the vpc e.g. '172.22.0.0/16'
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      natGateways: 0, // expensive -- we don't need that, yet (we have no PRIVATE subnets)
+      subnetConfiguration: [
+        {
+          cidrMask: 22,
+          name: `${props.prefix}-public-`,
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      ],
+    })
+    
+  }
+}
+
 wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.40/bin/apache-tomcat-9.0.40.tar.gz
 # Lấy các tham số đầu vào
 path=${1:-.}
