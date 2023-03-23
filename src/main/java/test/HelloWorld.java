2bcafe075b31f54e700d511bf31495d7a830011e
Nguyễn Tấn Quang <tanquanga9@gmail.com>
2023-03-23 07:58:25 +0700

    Update HelloWorld.java

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -1,4 +1,28 @@
-    subprocess.check_call(["id", username])
-    print(f"User {username} already exists")
-except subprocess.CalledProcessError:
-    print(f"User {username} does not exist - creating new user")
+import boto3
+
+def check_directory(instance_id, directory_path):
+    # Create EC2 client
+    ec2 = boto3.client('ec2')
+
+    # Describe instances
+    response = ec2.describe_instances(InstanceIds=[instance_id])
+
+    # Get instance
+    instance = response['Reservations'][0]['Instances'][0]
+
+    # Get volume ids
+    volume_ids = [v['Ebs']['VolumeId'] for v in instance['BlockDeviceMappings']]
+
+    # Create EC2 resource
+    ec2_resource = boto3.resource('ec2')
+
+    # Get volumes
+    volumes = ec2_resource.volumes.filter(VolumeIds=volume_ids)
+
+    # Check each mount target
+    for volume in volumes:
+        for mount_target in volume.attachments[0]['Volume']['Attachments']:
+            if mount_target['Device'] == directory_path:
+                return True
+
+    return False
