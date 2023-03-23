de4364af06dff8a480c3fa29e871c382d2c03248
Nguyễn Tấn Quang <tanquanga9@gmail.com>
2023-03-23 10:29:24 +0700

    Update HelloWorld.java

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -1,28 +1,34 @@
 import boto3
 
-def check_directory(instance_id, directory_path):
-    # Create EC2 client
-    ec2 = boto3.client('ec2')
+def lambda_handler(event, context):
+    instance_id = 'i-0123456789abcdef' # replace with your instance ID
+    folder_path = '/path/to/folder' # replace with your folder path
 
-    # Describe instances
-    response = ec2.describe_instances(InstanceIds=[instance_id])
+    # Create an SSM client
+    ssm = boto3.client('ssm')
 
-    # Get instance
-    instance = response['Reservations'][0]['Instances'][0]
+    # Send the command to the instance and get the command ID
+    response = ssm.send_command(
+        InstanceIds=[instance_id],
+        DocumentName='AWS-RunShellScript',
+        Parameters={
+            'commands': [
+                f'[ -d {folder_path} ] && echo True || echo False'
+            ]
+        }
+    )
+    command_id = response['Command']['CommandId']
 
-    # Get volume ids
-    volume_ids = [v['Ebs']['VolumeId'] for v in instance['BlockDeviceMappings']]
+    # Get the command output
+    output = ''
+    while output == '':
+        response = ssm.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instance_id
+        )
+        output = response.get('StandardOutputContent', '').strip()
 
-    # Create EC2 resource
-    ec2_resource = boto3.resource('ec2')
-
-    # Get volumes
-    volumes = ec2_resource.volumes.filter(VolumeIds=volume_ids)
-
-    # Check each mount target
-    for volume in volumes:
-        for mount_target in volume.attachments[0]['Volume']['Attachments']:
-            if mount_target['Device'] == directory_path:
-                return True
-
-    return False
+    if output == 'True':
+        print(f'The folder {folder_path} exists on instance {instance_id}')
+    else:
+        print(f'The folder {folder_path} does not exist on instance {instance_id}')
